41852d1900d44161886c3c6474c5368530edd2b6
lasserre <cstew33@gmail.com>
2022-01-19 15:26:25 -0600

    Added cmdline todos

--- wildebeest/scripts/cmdline.py
+++ wildebeest/scripts/cmdline.py
@@ -4,6 +4,25 @@ from pathlib import Path
 
 from wildebeest.jobrunner import run_job
 
+# Other wdb command line examples/ideas:
+# --------------------------------------
+# YES, we can add sub-nouns like "wdb run status" but since we usually will
+# be talking about experiments, let that be default/implied: "wdb [exp] status"
+# --------------------------------------
+# wdb create funcprotos ./fp.exp          # init registered experiment
+# wdb create funcprotos fp.exp --project-list RANDOM
+# wdb create funcprotos       # defaults to funcprotos.exp in cwd
+# wdb run fp.exp                        # run an experiment with current project list
+# wdb run fp.exp --project-list SPEC    # override project list
+# wdb run fp.exp -j 25                  # run this with up to 25 jobs (runs) at once
+# wdb kill fp.exp                       # TODO if needed
+# wdb export fp.exp                     # TODO if needed, creates fp.exp.tar.gz?
+# wdb export --data fp.exp              # exp definitions (exp.yaml), run status?, and data folder only
+# wdb status            # show a status table of all running experiments (# runs complete/total # runs, # jobs, etc.)
+# wdb status fp.exp     # more detailed status for this exp?
+# wdb status fp.exp --run run1      # run status
+# wdb log fp.exp run1               # dump the log from this run, we can combine with watch/tail to make this nice
+
 def main():
     p = argparse.ArgumentParser(description='Runs wildebeest commands')
 
