b1699015c60fac9a8c0947cbab56f85555a1ace7
lasserre <cstew33@gmail.com>
2023-05-21 21:52:19 -0500

    testing path

--- wildebeest/buildsystemdrivers/cmakedriver.py
+++ wildebeest/buildsystemdrivers/cmakedriver.py
@@ -9,10 +9,14 @@ class CmakeDriver(BuildSystemDriver):
 
     def _do_configure(self, runconfig: RunConfig, build: ProjectBuild):
         configure_opts = build.recipe.configure_options.cmdline_options
+
+        # TODO: TEST THIS - IS MY PATH RIGHT HERE??
+        subprocess.run(['echo CALEB TEST: PATH=$PATH'], shell=True)
         subprocess.run(['cmake', build.project_root, *configure_opts])
 
     def _do_build(self, runconfig: RunConfig, build: ProjectBuild, numjobs:int = 1):
         build_opts = build.recipe.build_options.cmdline_options
+        subprocess.run(['echo CALEB TEST: PATH=$PATH'], shell=True)
         p = subprocess.run(['cmake', '--build', '.', f'-j{numjobs}', *build_opts])
         if p.returncode != 0:
             raise Exception(f'cmake build failed with return code {p.returncode}')
