e6a9f9d55775b5aa6f04d022fa6d36ddb1234378
lasserre <cstew33@gmail.com>
2024-01-21 19:24:55 -0600

    Added texinfo to binutils prereqs

--- wildebeest/recipes/benchmarks.py
+++ wildebeest/recipes/benchmarks.py
@@ -27,6 +27,7 @@ binutils_v2_36 = CreateProjectRecipe(git_remote='git://sourceware.org/git/binuti
     git_head='binutils-2_36',
     source_languages=[LANG_C],
     out_of_tree=True,   # not sure...
+    apt_deps = ['texinfo'],
 )
 
 benchmark_recipes = [
