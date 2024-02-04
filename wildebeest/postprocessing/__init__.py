471a993ec9fc340b3a4de1008bf42c2fc859466c
lasserre <cstew33@gmail.com>
2024-02-04 15:01:24 -0600

    Added file/find-based method of locating binaries  since we were missing some for the benchmarks

--- wildebeest/postprocessing/__init__.py
+++ wildebeest/postprocessing/__init__.py
@@ -1,3 +1,3 @@
-from .llvm_instrumentation import *
+from . import llvm_instrumentation
 from .ghidra import *
 from .flatlayoutbinary import *
--- wildebeest/postprocessing/flatlayoutbinary.py
+++ wildebeest/postprocessing/flatlayoutbinary.py
@@ -3,11 +3,12 @@ from pathlib import Path
 import re
 import shutil
 import subprocess
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from ..run import Run
 from ..experimentalgorithm import RunStep
 from ..runconfig import recognized_opt_levels
+from .llvm_instrumentation import is_cmake_generated
 
 class FlatLayoutBinary:
     def __init__(self, binary_id:int, binary_file:Path, linker_objs:Path,
@@ -32,6 +33,82 @@ class FlatLayoutBinary:
         step-specific.
         '''
 
+def elf_has_debuginfo(elf:Path) -> bool:
+    rval = subprocess.run(f'readelf -t {elf} | grep debug_info > /dev/null', shell=True).returncode
+    return bool(rval == 0)
+
+def find_binaries_in_path(build_path:Path, no_cmake:bool) -> List[Path]:
+    # this finds all file that can be executed (no static libraries..)
+    raw_output = subprocess.check_output(f'find {build_path} -type f -executable -print | xargs file | grep "ELF 64-bit"', shell=True).decode('utf-8')
+    raw_lines = [x for x in raw_output.split('\n') if x]
+
+    regex_results = [re.match('(.*):\s+ELF 64-bit.*', rl) for rl in raw_lines]
+    elf_files = [Path(res.groups()[0]) for res in regex_results if res]
+
+    # now verify they have debug info...
+    debug_elfs = [elf for elf in elf_files if elf_has_debuginfo(elf)]
+
+    if no_cmake:
+        debug_elfs = [x for x in debug_elfs if not is_cmake_generated(Path(x))]
+
+    for elf in debug_elfs:
+        print(f'File {Path(elf)} has debug info')
+
+    # import IPython; IPython.embed()
+    return debug_elfs
+
+def find_binaries_main():
+    import argparse
+    p = argparse.ArgumentParser(description='Find executables in the given build folder')
+    p.add_argument('build_folder')
+    p.add_argument('--no-cmake', help='Exclude cmake-generated binaries', action='store_true')
+    args = p.parse_args()
+    find_binaries_in_path(Path(args.build_folder), bool(args.no_cmake))
+    return 0
+
+def calc_percent_cpp_names_in_binary(elf:Path) -> float:
+    total_syms = subprocess.check_output(f'nm {elf} | wc -l', shell=True).decode('utf-8')
+    cpp_syms = subprocess.check_output(f'nm {elf} | c++filt | grep "::" | wc -l', shell=True).decode('utf-8')
+
+    if int(total_syms) > 0:
+        return int(cpp_syms)/int(total_syms)
+    return -1
+
+def is_cpp_debug_binary(elf:Path, cppnames_thresh:float=0.65) -> bool:
+    '''
+    cppnames_thresh: Threshold for % of C++ symbol names in the debug binary which
+                     flag a binary as being a "C++ binary"
+    '''
+    percent_cpp = calc_percent_cpp_names_in_binary(elf)
+    if percent_cpp > -1:
+        print(f'{percent_cpp*100:.2f}% of symbols are C++ syms in {elf.name}')
+        return percent_cpp > cppnames_thresh
+    else:
+        print(f'No symbols in binary {elf}')
+        return False
+
+def _do_find_binaries(run:Run, params:Dict[str,Any], outputs:Dict[str,Any]):
+    debug_bins = find_binaries_in_path(run.build.build_folder, no_cmake=True)
+    good_bins = [x for x in debug_bins if validate_optimization_level(run, x)]
+
+    return {
+        'binaries': good_bins
+    }
+
+def find_binaries() -> RunStep:
+    '''
+    Creates a RunStep that will find binaries that are 64-bit ELF files
+    and contain a debug_info section
+
+    Outputs
+    -------
+    The output of this step is a dictionary with this format:
+    {
+        'binaries': [ list of corresponding binary paths ],
+    }
+    '''
+    return RunStep('find_binaries', _do_find_binaries)
+
 def _do_flatten_binaries(run:Run, params:Dict[str,Any], outputs:Dict[str,Any]):
     if 'find_binaries' not in outputs:
         raise Exception('Need the find_binaries step to be run first')
@@ -40,7 +117,9 @@ def _do_flatten_binaries(run:Run, params:Dict[str,Any], outputs:Dict[str,Any]):
     bdict = {}
     for i, b in enumerate(binaries):
         lobj = b.with_suffix('.linker-objects')
+        lobj = lobj if lobj.exists() else None
         bdict[i] = FlatLayoutBinary(i, b, lobj, run)
+        bdict[i].data['percent_cpp'] = calc_percent_cpp_names_in_binary(b)
 
     df = pd.DataFrame([vars(fb) for fb in bdict.values()])
     df.to_csv(run.data_folder/'flat_layout.csv', index=False)
@@ -83,8 +162,12 @@ def flatten_binaries() -> RunStep:
     '''
     return RunStep('flatten_binaries', _do_flatten_binaries)
 
-def validate_optimization_level(run:Run, binfile:Path):
-    '''Validate that no other optimization levels appear in DW_AT_producer strings in the binary's DWARF info'''
+def validate_optimization_level(run:Run, binfile:Path) -> bool:
+    '''
+    Validate that no other optimization levels appear in DW_AT_producer strings in the binary's DWARF info
+
+    Returns True if binary matches desired level, False otherwise
+    '''
     dw_at_producer = subprocess.check_output(f'readelf --debug-dump=info {binfile} | grep DW_AT_producer', shell=True).decode('utf-8')
     other_levels = [x for x in recognized_opt_levels() if x != run.config.opt_level]
 
@@ -92,9 +175,11 @@ def validate_optimization_level(run:Run, binfile:Path):
     # that no other flags appear. If we can't avoid multiple in the future, we can add logic to
     # validate that our flag always appears last in the multiple flags case
 
-    m = re.match(f'.*({"|".join(other_levels)}).*', dw_at_producer)
-    if m:
-        raise Exception(f'Binary {binfile} compiled with different optimization than configured (opt_level={run.config.opt_level}). Found {m.groups()[0]}')
+    found_others = re.match(f'.*({"|".join(other_levels)}).*', dw_at_producer)
+    if found_others:
+        print(f'{binfile} compiled with unwanted optimization level (opt_level={run.config.opt_level}). Found {found_others.groups()[0]}')
+
+    return bool(not found_others)
 
 def _do_strip_binaries(run:Run, params:Dict[str,Any], outputs:Dict[str,Any]):
     # STRIP:
@@ -119,8 +204,6 @@ def _do_strip_binaries(run:Run, params:Dict[str,Any], outputs:Dict[str,Any]):
         fb.data['debug_binaries'] = origcopy
         fb.debug_binary_file = origcopy
         fb.stripped_binary_file = stripped
-
-        validate_optimization_level(run, origcopy)      # can't validate on stripped since we need debug info
 
     # import IPython; IPython.embed()
 
