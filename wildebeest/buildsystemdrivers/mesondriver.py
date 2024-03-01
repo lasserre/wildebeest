6f5637226a2f84b65ca40ed99b91d4bf35ef3bbf
lasserre <cstew33@gmail.com>
2024-02-29 21:51:55 -0600

    Fixed config_opts usage

--- wildebeest/buildsystemdrivers/mesondriver.py
+++ wildebeest/buildsystemdrivers/mesondriver.py
@@ -8,17 +8,18 @@ class MesonDriver(BuildSystemDriver):
         super().__init__('meson')
 
     def _do_configure(self, runconfig: RunConfig, build: ProjectBuild, **kwargs):
-        opts = build.recipe.configure_options.cmdline_options
+        config_opts = build.recipe.configure_options
+        cmdline_opts = config_opts.cmdline_options
 
         # let runconfig generate the flags strings so that we do it the same way
         # as other build systems (which just read the env vars)
-        configure_env = runconfig.generate_env(opts.extra_cflags, opts.extra_cxxflags, opts.linker_flags)
+        configure_env = runconfig.generate_env(config_opts.extra_cflags, config_opts.extra_cxxflags, config_opts.linker_flags)
         cflags = configure_env['CFLAGS']
         cxxflags = configure_env['CXXFLAGS']
         ldflags = configure_env['LDFLAGS'] if 'LDFLAGS' in configure_env else ''
 
         cmdline = f'meson setup --buildtype=plain -Dc_args="{cflags}" -Dcpp_args="{cxxflags}" -Dc_link_args="{ldflags}" -Dcpp_link_args="{ldflags}" '\
-            f'{build.project_root} {" ".join(str(x) for x in opts)}'
+            f'{build.project_root} {" ".join(str(x) for x in cmdline_opts)}'
 
         print(f'meson commandline: {cmdline}')
         subprocess.run(cmdline, shell=True)
